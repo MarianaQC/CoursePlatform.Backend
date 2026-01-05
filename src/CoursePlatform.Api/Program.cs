17f624c18b852b5f80a38f862c0e1b48ffc15282
MarianaQC <marianaqc64@gmail.com>
2026-01-05 12:44:44 -0500

    Fix Program.cs syntax error

--- src/CoursePlatform.Api/Program.cs
+++ src/CoursePlatform.Api/Program.cs
@@ -1,27 +1,14 @@
-private static IServiceCollection AddCorsConfiguration(this IServiceCollection services)
-{
-    services.AddCors(options =>
-    {
-        options.AddPolicy("AllowFrontend", builder =>
-        {
-            builder.WithOrigins(
-                    "http://localhost:3000",
-                    "http://localhost:5173",
-                    "https://localhost:3000",
-                    "https://localhost:5173"
-                )
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .AllowCredentials();
-        });
-        
-        options.AddPolicy("AllowAll", builder =>
-        {
-            builder.AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader();
-        });
-    });
+using CoursePlatform.Api.Extensions;
 
-    return services;
-}
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+
+var app = builder.Build();
+
+await app.UseApplicationMiddleware(app.Environment);
+
+app.MapControllers();
+
+app.Run();
